76351b8d86a8771ac105136ac127f3e9bf6591dd
zorichDev <ffranzor@yahoo.com>
2025-04-09 11:57:08 +0100

    transition error

--- src/components/IncomeSummary.jsx
+++ src/components/IncomeSummary.jsx
@@ -286,7 +286,6 @@ const IncomeSummary = () => {
             transition="all 0.3s"
             initial={{ opacity: 0, y: 20 }}
             animate={isLoaded ? { opacity: 1, y: 0 } : {}}
-            transition={{ duration: 0.5, delay: 0.1 * idx }}
           >
             <Flex align="center" mb={3}>
               <Flex
